e9604c72e20e9d121e8b084e993a75d0717316bf
İshak Eren <ishakerenn1905@gmail.com>
2025-01-03 23:58:09 +0300

    Button_Designed

--- Projects/bolum-4-canavar-avi-oyunu-template/app.js
+++ Projects/bolum-4-canavar-avi-oyunu-template/app.js
@@ -10,7 +10,11 @@ new Vue({
             this.game_is_on = true;
         },
         attack: function(){
-
+            var point = Math.ceil(Math.random() * 10);
+            this.monster_heal -= point;
+            this.monster_attack();
+            console.log("M :" + this.monster_heal );
+            console.log("P :" + this.player_heal);
         },
         special_attack: function(){
 
@@ -20,6 +24,10 @@ new Vue({
         },
         give_up: function(){
 
+        },
+        monster_attack: function(){
+             var point = Math.ceil(Math.random() * 10);
+            this.player_heal -= point;
         }
     }
 })
