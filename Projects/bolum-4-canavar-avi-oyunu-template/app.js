e66808d72f2e61a35a5d230c080fa0837f49e110
İshak Eren <ishakerenn1905@gmail.com>
2025-01-04 23:00:54 +0300

    Monster_Hunting_Game_Finished

--- Projects/bolum-4-canavar-avi-oyunu-template/app.js
+++ Projects/bolum-4-canavar-avi-oyunu-template/app.js
@@ -4,6 +4,7 @@ new Vue({
         player_heal: 100,
         monster_heal:100,
         game_is_on: false,
+        logs: [],
     },
     methods: {
         start_game: function(){
@@ -13,31 +14,46 @@ new Vue({
             var point = Math.ceil(Math.random() * 10);
             this.monster_heal -= point;
             this.monster_attack();
+            this.add_to_log({turn: "p", text : "OYUNCU ATAĞI (" + point +")" });
             console.log("M :" + this.monster_heal );
             console.log("P :" + this.player_heal);
         },
         special_attack: function(){
             var point = Math.ceil(Math.random() * 25);
+            this.add_to_log({turn: "p", text : "OYUNCU ÖZEL ATAĞI (" + point +")" });
             this.monster_heal -= point;
             this.monster_attack();
+
         },
         heal_up: function() {
             var point = Math.ceil(Math.random() * 27);
+            this.add_to_log({turn: "p", text : "OYUNCU İLK YARDIM ALDI (" + point +")" });
             this.player_heal += point;
             this.monster_attack();
         },
         give_up: function(){
             this.player_heal = 0;
+            this.add_to_log({turn: "p", text : "OYUNCU PES ETTİ" });
+
         },
         monster_attack: function(){
              var point = Math.ceil(Math.random() * 15);
+            this.add_to_log({turn: "m", text : "CANAVAR SALDIRISI (" + point +")" });
             this.player_heal -= point;
+        },
+        add_to_log: function(log){
+            this.logs.push(log)
         }
     },
     watch: {
         player_heal : function(value){
             if(value <= 0){
                 this.player_heal = 0
+                if(confirm("Oyunu Kaybettin! Tekrar Denemek ister misin?")){
+                    this.player_heal = 100;
+                    this.monster_heal = 100;
+                    this.logs = [];
+                }
             }
             else if (value >= 100){
                 this.player_heal = 100
@@ -46,6 +62,12 @@ new Vue({
          monster_heal : function(value){
             if(value <= 0){
                 this.monster_heal = 0
+                if(confirm("Oyunu Kazandın! Tekrar Denemek ister misin?")){
+                    this.player_heal = 100;
+                    this.monster_heal = 100;
+                    this.logs = [];
+                }
+                
             }
         }
     }
