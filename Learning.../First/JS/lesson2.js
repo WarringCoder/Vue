3f575ce5631c7a3d310e510b15325caff8465bc6
İshak Eren <ishakerenn1905@gmail.com>
2025-01-11 22:18:24 +0300

    Components_entry

---
+++ Learning.../First/JS/lesson2.js
@@ -0,0 +1,22 @@
+Vue.component('mycomp', {
+    data:function(){
+        return{
+            status:"pasif"
+        }
+    },
+    template: '<h1>Componentlere Giriş</h1>'
+});
+
+new Vue({
+    el: "#component",
+
+});
+
+
+
+
+
+
+
+
+
