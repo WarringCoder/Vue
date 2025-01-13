049992fbcb7dd58faa0d99a4d07eaeb5c4247ba6
İshak Eren <ishakerenn1905@gmail.com>
2025-01-13 19:53:52 +0300

    Components_Learned

---
+++ Learning.../bolum-8-componentler-arasi-iletisim-baslangic-kodu/src/components/User.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <h1>Parent Component (User)</h1>
+    <p>Burası parent component yani herşeyin import edildiği component :)</p>
+    <button @click="changed"> İsmi Değiştir</button>
+    <p>{{ childData }}</p>
+    <p>{{ age }}</p>
+    <hr>
+    <div class="row">
+      <app-user-detail  :sendAge="age" @data="childData = $event" :name="title"></app-user-detail>
+      <app-user-edit :sendAge="age" @newAgee="age = $event"></app-user-edit>
+    </div>
+  </div>
+</template>
+<script>
+  import UserDetail from "./UserDetail";
+  import UserEdit from "./UserEdit";
+  export default {
+    components : {
+      appUserDetail : UserDetail,
+      appUserEdit : UserEdit,
+    },
+    data: function(){
+      return{
+        title:"İshak",
+        childData:"",
+        age:25
+      }
+    },
+    methods:{
+      changed(){
+        if(this.title === "İshak")
+        {
+          this.title = "Firdevs"
+        }
+        else
+        {
+          this.title = "İshak"
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  div.container{
+    margin-top: 30px;
+    padding: 20px 40px;
+    background-color: #6a8d99;
+    border: 1px solid #666;
+  }
+  div.row {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+</style>
---
+++ Learning.../bolum-8-componentler-arasi-iletisim-baslangic-kodu/src/components/UserDetail.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="wcol-md-6">
+    <h3>Child Component 1</h3>
+    <p>Ben User.vue isimli Parent Component'in içerisindeki bir Child componentim</p>
+    <p>Kullanıcı Adı: {{ name }}</p>
+    <p>Ters Hali: {{ switchName() }}</p>
+    <p>Yaş: {{ sendAge }}</p>
+    <button @click="sendToParent">Child Component'ten Parent'e Veri Gönder</button>
+  </div>
+</template>
+<script>
+  export default{
+    props:{
+      name:{
+        type: String, Number,
+        default:"Birbirini İyileştiren İki Güç "
+      },
+      sendAge:{
+        type:Number
+      }
+    },
+    methods:{
+      switchName(){
+        return  this.name.split("").reverse().join("");   
+      },
+      sendToParent()
+      {
+        this.$emit("data", "Warring Coder")
+      }
+    }
+  }
+</script>
+<style scoped>
+  div {
+    background-color: lightcoral;
+    padding: 20px;
+    border: 1px solid #666;
+    display: inline-block;
+  }
+</style>
