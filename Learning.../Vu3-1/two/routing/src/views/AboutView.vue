48d08c32b3f450139ea147c1661bb30982edd4b5
İshak Eren <ishakerenn1905@gmail.com>
2025-01-25 00:26:54 +0300

    Vue_routing_Lesson_two_completed

--- Learning.../Vu3-1/two/routing/src/views/AboutView.vue
+++ Learning.../Vu3-1/two/routing/src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About Page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, perspiciatis. Quia asperiores odit quibusdam numquam cumque hic at doloremque placeat! Illum velit rem dicta dolor aliquam. Facilis dolores eum ipsa, officiis quisquam, dolorum nam dicta minus nihil voluptate eaque sunt cupiditate fugit exercitationem aperiam. Temporibus debitis nesciunt qui veniam cum?</p>
   </div>
 </template>
--- Learning.../Vu3-1/two/routing/src/views/HomeView.vue
+++ Learning.../Vu3-1/two/routing/src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <h2>Ana Sayfa</h2>
+    <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga incidunt laboriosam aliquid minima culpa sit tempora aut distinctio officia. Quos, obcaecati labore! Ab debitis dolores magnam nesciunt vero officiis nisi dolore commodi.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos nulla totam expedita ducimus ut est repellendus necessitatibus atque laboriosam voluptatibus, quasi nisi natus veritatis quae consequatur? Ullam unde possimus magnam animi. Nesciunt expedita explicabo nemo obcaecati velit sint iusto veniam assumenda sit!</p>
+ </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>
