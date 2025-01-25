233f29f8b4aa72379bbe3bf0ebc503f92de3f9e1
İshak Eren <ishakerenn1905@gmail.com>
2025-01-26 01:45:09 +0300

    Routing_Lesson_Three_Completed

---
+++ Learning.../Vu3-1/two/routing/src/views/StudentView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <h2>Öğrencilerim</h2>
+    <div v-for='student in students' :key="student.id">
+        <h4>{{ student.name }}</h4>
+    </div>
+ </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'StudentView',
+  components: {
+  },
+  data(){
+    return{
+        students:
+        [
+            {name:"İshak Eren", id:1},
+            {name:"Firdevs Rana", id:2}
+        ]
+    }   
+  }
+}
+</script>
