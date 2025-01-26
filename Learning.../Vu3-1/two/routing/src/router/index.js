6474cf4378b1c8b0daec4bf7639cd0aef2e45c90
İshak Eren <ishakerenn1905@gmail.com>
2025-01-27 02:24:12 +0300

    Route_Lesson_Completed

--- Learning.../Vu3-1/two/routing/src/router/index.js
+++ Learning.../Vu3-1/two/routing/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue';
 import AboutView from '@/views/AboutView.vue';
 import StudentView from '@/views/StudentView.vue';
+import StudentDetail from '@/views/StudentDetail.vue';
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: '/student',
     name: 'StudentView',
     component: StudentView
+  },
+  {
+    path: '/student/:id',
+    name: 'StudentDetail',
+    component: StudentDetail
   }
 ]
 
---
+++ Learning.../Vu3-1/two/routing/src/views/StudentDetail.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>Öğrenci Detay</h2>
+        <h4>{{ id }}</h4>
+    </div>
+</template>
+<script>
+    export default
+    {
+        data(){
+            return{
+                id:this.$route.params.id
+            }
+        }
+    }
+</script>
