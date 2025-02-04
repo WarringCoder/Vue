2a2aab7bfc12faeee48808e4c2084bb88f3f0732
İshak Eren <ishakerenn1905@gmail.com>
2025-02-05 01:56:53 +0300

    Data_Building_Projects

--- Projects/jira/src/App.vue
+++ Projects/jira/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link>
+    <router-link to="/">Home</router-link>  
   </nav>
   <router-view/>
 </template>
@@ -9,7 +9,7 @@
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  -moz-osx-font-smoothing: grayscale; 
+ 
 }
 </style>
