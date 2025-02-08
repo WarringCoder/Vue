2a1e8cc624eadb1371a68f6bb4d141a8373b70d7
İshak Eren <ishakerenn1905@gmail.com>
2025-02-09 02:08:56 +0300

    Composition_Api_started

---
+++ Learning.../Composition_1/composition/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div> 
+    <p><strong>ad:</strong>  {{ mösyö}}</p> 
+    <p><strong> yaş:</strong> {{ age}}</p> 
+    <p><strong>iş:</strong>  {{ job}}</p> 
+    <p><strong>maaş:</strong>  {{ wage}}K</p> 
+    <button @click="üstseviye">yüksel</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  setup(){
+    let mösyö= "İshak Eren";
+    let age= 22;
+    let job = "software developer";
+    let wage = 50;
+
+    const üstseviye = () =>{
+      alert("Savaşçı Kazandı ve Yükselmeye Devam Ediyor");
+    }
+
+    return {mösyö, age, job, wage, üstseviye}
+  }
+}
+</script>
+
+<style>
+
+</style>
